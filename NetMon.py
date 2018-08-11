94da55ee8b1b7beeb71fdaf42484c7d00ce40c0e
Dustin <dustin.grady@gmail.com>
2018-08-11 03:35:55 -0700

    Fixed threading bug between mainloop/ scan_Network()

--- NetMon.py
+++ NetMon.py
@@ -5,6 +5,7 @@
 
 from tkinter import Tk, Label, Button
 import threading
+import queue
 import os
 
 class NetworkMonitor:
@@ -12,30 +13,47 @@ class NetworkMonitor:
         self.master = master
         master.title('Network Monitor v0.1')
 
-        self.greet_button = Button(master, text="Scan", command=self.scan_Network)
-        self.greet_button.grid(row=0, column=1)
-
-        self.close_button = Button(master, text="Close", command=master.quit)
-        self.close_button.grid(row=1, column=1)
-
         self.THREAD_COUNT = 8
-        self.threads = {}
-        for i in range(0, self.THREAD_COUNT-1):
-            self.threads['thread' + str(i)] = threading.Thread(target=lambda: self.scan_Network) #Create thread
-            self.threads['thread' + str(i)].start() #Start thread
+
+        self.addrQueue = queue.Queue()
+        [self.addrQueue.put(i) for i in ['192.168.' + str(x) + '.' + str(y) for x in range(0, 256) for y in range(0, 256)]]
+
+        self.thread = threading.Thread(target=self.scan_Network)
+        self.thread.daemon = True
+
+        self.build_GUI()
+
+        #self.threads = {}
+        #for i in range(0, self.THREAD_COUNT):
+        #    self.threads['thread' + str(i)] = threading.Thread(target=self.scan_Network) #Create thread
+        #    self.threads['thread' + str(i)].start() #Start thread
+
+    '''Draw GUI elements'''
+    def build_GUI(self):
+        #self.scan_button = Button(self.master, text="Scan", command=self.scan_Network)
+        self.scan_button = Button(self.master, text="Scan", command=lambda: self.thread.start())
+
+        self.scan_button.grid(row=0, column=1)
+
+        self.close_button = Button(self.master, text="Close", command=self.master.quit)
+        self.close_button.grid(row=1, column=1)
 
     '''Using 16-bit IPv4 schema'''
     def scan_Network(self):
-        for x in range(0, 256):
-            for y in range(0, 256):
-                addr = '192.168.'+str(x)+'.'+str(y)
-                print("Threads: " + str(threading.active_count())) #Testing
-                #addr = '192.168.1.232' #Testing
+        while not self.addrQueue.empty():
+            addr = self.addrQueue.get()
+            try:
+                print('Address: ', addr)  #Testing
+                print("Threads: " + str(threading.active_count()))  #Testing
                 response = os.system("ping -n 1 " + addr)
                 if response == 0:
                     print(addr, 'is up!')
                 else:
                     print(addr, 'is down!')
+                self.addrQueue.put(addr)
+                print('Size: ', self.addrQueue.qsize()) #Testing
+            except:
+                print('Error during scan')
 
 root = Tk()
 app = NetworkMonitor(root)
